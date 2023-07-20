4ac57fa2ed552e208a309a562f6fa2e6f7d1aea9
Leonardo <jlobando2@espe.edu.ec>
2023-07-19 20:34:54 -0500

    sistema

---
+++ js/enlaces_internos.js
@@ -0,0 +1,134 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('opcion_quienes').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/quienes_somos.html', true);
+        xhr.send();
+    });
+
+    //galeria compra
+    document.getElementById('comp_casa').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/casa.html', true);
+        xhr.send();
+    });
+
+    document.getElementById('comp_depar').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/depa.html', true);
+        xhr.send();
+    });
+    
+    document.getElementById('comp_terreno').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/terreno.html', true);
+        xhr.send();
+    });
+
+    //galeria arrriendo
+    document.getElementById('arriendo_casa').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/casa.html', true);
+        xhr.send();
+    });
+
+    document.getElementById('arriendo_depar').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/depa.html', true);
+        xhr.send();
+    });
+    
+    document.getElementById('arriendo_terreno').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/terreno.html', true);
+        xhr.send();
+    });
+
+    //pie de pagina
+    document.getElementById('politicas').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/poli_priv.html', true);
+        xhr.send();
+    });
+    
+    document.getElementById('terminos').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/term_condi.html', true);
+        xhr.send();
+    });
+
+    document.getElementById('cookies').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'sis_html/cookies.html', true);
+        xhr.send();
+    });
+});
