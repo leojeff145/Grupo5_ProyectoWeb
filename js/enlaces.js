089c57aa692b31965e8f078e6e1f3fbf33f8ef84
SebastianSolano <sisolano@espe.edu.ec>
2023-07-20 01:20:18 -0500

    inicio

--- js/enlaces.js
+++ js/enlaces.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('inicio').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.getElementById('mostrar').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', 'html/document.html', true);
+        xhr.send();
+    });
+	
     document.getElementById('opcion_quienes').addEventListener('click', function (event) {
         event.preventDefault();
 
